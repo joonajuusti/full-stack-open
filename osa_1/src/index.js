acae77e74cb7e8100a54f2e9fe7715b554340c08
Joona Juusti <joona.juusti@gmail.com>
2018-08-03 23:52:27 +0300

    1.4 objects to array

--- osa_1/src/index.js
+++ osa_1/src/index.js
@@ -16,19 +16,19 @@ const Osa = props => {
 }
 
 const Sisalto = props => {
-  const { osa1, osa2, osa3 } = props
+  const { osat } = props
   return (
     <div>
-      <Osa osa={osa1} />
-      <Osa osa={osa2} />
-      <Osa osa={osa3} />
+      <Osa osa={osat[0]} />
+      <Osa osa={osat[1]} />
+      <Osa osa={osat[2]} />
     </div>
   )
 }
 
 const Yhteensa = props => {
-  const { tehtavia1, tehtavia2, tehtavia3 } = props
-  const tehtaviaYhteensa = tehtavia1 + tehtavia2 + tehtavia3
+  const { tehtavat } = props
+  const tehtaviaYhteensa = tehtavat[0] + tehtavat[1] + tehtavat[2]
   return (
     <p>yhteensä {tehtaviaYhteensa} tehtävää</p>
   )
@@ -36,24 +36,26 @@ const Yhteensa = props => {
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = {
-    nimi: 'Reactin perusteet',
-    tehtavia: 10
-  }
-  const osa2 = {
-    nimi: 'Tiedonvälitys propseilla',
-    tehtavia: 7
-  }
-  const osa3 = {
-    nimi: 'Komponenttien tila',
-    tehtavia: 14
-  }
+  const osat = [
+    {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    },
+    {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    },
+    {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  ]
 
   return (
     <div>
       <Otsikko otsikko={kurssi}/>
-      <Sisalto osa1={osa1} osa2={osa2} osa3={osa3}/>
-      <Yhteensa tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia}/>
+      <Sisalto osat={osat}/>
+      <Yhteensa tehtavat={osat.map(osa => osa.tehtavia)}/>
     </div>
   )
 }
