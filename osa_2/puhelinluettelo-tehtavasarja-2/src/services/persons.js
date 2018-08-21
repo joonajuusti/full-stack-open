91b7704a8fea68b4b4732159479f17f6f5de456f
Joona Juusti <joona.juusti@gmail.com>
2018-08-21 19:20:15 +0300

    3.9

--- osa_2/puhelinluettelo-tehtavasarja-2/src/services/persons.js
+++ osa_2/puhelinluettelo-tehtavasarja-2/src/services/persons.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
-const API_URL = 'http://localhost:3001/persons'
+const API_URL = 'http://localhost:3001/api/'
 
 const getAll = () => {
-  const request = axios.get(API_URL)
+  const request = axios.get(`${API_URL}persons`)
   return request.then(response => response.data)
 }
 
 const create = newPerson => {
-  const request = axios.post(API_URL, newPerson)
+  const request = axios.post(`${API_URL}persons`, newPerson)
   return request.then(response => response.data)
 }
 
 const deletePerson = id => {
-  const request = axios.delete(`${API_URL}/${id}`)
+  const request = axios.delete(`${API_URL}persons/${id}`)
   return request.then(response => response.data)
 }
 
 const update = (id, newPerson) => {
-  const request = axios.put(`${API_URL}/${id}`, newPerson)
+  const request = axios.put(`${API_URL}persons/${id}`, newPerson)
   return request.then(response => response.data)
 }
 
